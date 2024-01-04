91431b0f973e127c3c81f02436367cd5627fdf6e
Kelvin Javorski <kelvin.javorskisoares@gmail.com>
2024-01-04 11:53:36 -0300

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Kelvin Javorski Soares on 04/01/24.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
